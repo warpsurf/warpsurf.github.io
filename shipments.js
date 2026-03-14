596fa4620d42ceb896073f2f3a4f42f7dda02185
jonathanroberts <jdr53@cam.ac.uk>
2026-03-14 15:44:08 +0000

    Updating shipping log

--- shipments.js
+++ shipments.js
@@ -4,6 +4,11 @@ var SHIPMENTS = {
     {
       "date": "2026-03-13",
       "feature": "Search engine preference",
+      "tweet": "https://x.com/JRobertsAI/status/2032521455250854082"
+    },
+    {
+      "date": "2026-03-14",
+      "feature": "Microsoft Edge compatibility",
       "tweet": ""
     }
   ]
